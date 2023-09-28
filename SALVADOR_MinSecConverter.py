a46d3371101f057fbc4f90c0f29f6bdcb22f3d34
Malcrond <144977027+Malcrond@users.noreply.github.com>
2023-09-28 06:17:09 -0700

    Add files via upload

---
+++ SALVADOR_MinSecConverter.py
@@ -0,0 +1,4 @@
+rawnumber = int(input("Enter a number: "))
+minutes = rawnumber // 60
+seconds = rawnumber % 60
+print(str(rawnumber) + " " + "seconds is " + str(minutes) + " minutes and " + str(seconds) + " seconds")
